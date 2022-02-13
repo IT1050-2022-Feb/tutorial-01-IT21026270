9a43e541118350b6254f5c67d123883bc3f62d18
Nethmi Senarathne <99447197+IT21026270@users.noreply.github.com>
2022-02-13 19:44:24 +0000

    Tute 01

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,17 @@
 #include <stdio.h>
 
 int main() {
+  int mark1, mark2;
+  double average;
+  
+  printf("Enter marks for subject 1: ");
+  scanf("%d", &mark1);
+  printf("Enter marks for subject 2: ");
+  scanf("%d", &mark2);
+
+  average = (mark1 + mark2) / 2.0;
+
+  printf("Average = %.2f", average);
   
   return 0;
 }
